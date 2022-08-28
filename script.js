01652608de23dff58c1401fb20d1294e90f6e617
KhuloodBatis <k.batis@bimventures.com>
2022-08-28 17:29:06 +0300

    koolend

--- script.js
+++ script.js
@@ -19,8 +19,109 @@
 
 //?3
 
-import add, { cart } from './shoppingCart.js';
-add('bread', 5);
-add('pizza', 8);
-add('apples', 10);
-console.log(cart);
+// import add, { cart } from './shoppingCart.js';
+// add('bread', 5);
+// add('pizza', 8);
+// add('apples', 10);
+// console.log(cart);
+//? API 
+//?get all posts
+// console.log('start fetching');
+// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+// const data = await res.json()
+// console.log(data);
+
+// console.log('Something');
+
+//?get last Post 
+
+// const getLastPost = async function(){
+//     const res=await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+// const data = await res.json()
+// // console.log(data);
+
+// //? to get last post
+// return { title:data.at(-1).title, text: data.at(-1).body}
+
+// };
+//this return promise 
+// const lastPost = getLastPost();
+// console.log(lastPost);
+// not very clean 
+// lastPost.then(last=> console.log(last))
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+const ShoppingCart2 = (function (){
+const cart = [];
+const shippingCost = 10 ;
+const totalPrice = 237;
+const totalQuantity = 23;
+
+const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart supplier(shippingCost is ${shippingCost})`);
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from `);
+  };
+
+  return{
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity
+  }
+})();
+
+// ShoppingCart2.addToCart('apple',4);
+// ShoppingCart2.addToCart('pizza',2);
+// console.log(ShoppingCart2);
+
+//! commonJS Modules
+//Export
+// export.addToCart =  function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart supplier(shippingCost is ${shippingCost})`);
+//   };
+
+  //Import
+//   const {addToCart} = require('./shoppingCart.js')
+
+
+//! NPM lodash
+
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+// import cloneDeep from 'lodash-es';
+
+// const state = {
+//     cart:[{ product: 'bread', quantity:5},
+// {product: 'pizza', quantity:5}],
+// user:{loggedIn: true}
+// }
+
+// const stateClone = Object.assign({} , state);
+// const stateDeepClone = cloneDeep(state);
+
+// state.user.loggedIn = false;
+// console.log(stateClone);
+// console.log(stateDeepClone);
+//! npm parcel
+
+// if(!module.hot){
+//     module.hot.accept();
+// }
+
+//!
+class Person{
+    #greeting = 'Hey';
+    constructor(name){
+        this.name = name;
+        console.log(`${this.#greeting},${this.name}`);
+    }
+}
+
+const jonas = new Person('Jonas')
